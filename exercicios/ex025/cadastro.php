33ae10f370b4cf82090825aad6c779c96562bf61
Edmilson Monteiro Rodrigues Neto <62777025+EdmilsonRodrigues@users.noreply.github.com>
2023-12-22 20:53:41 +0000

    forms

---
+++ exercicios/ex025/cadastro.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro PHP</title>
+</head>
+<body>
+    <p>Os dados foram enviados</p>
+    <p>Aprenda PHP para saber o que fazer com eles</p>
+    <button onclick="javascript:window.history.go(-1)">Voltar</button>
+</body>
+</html>
